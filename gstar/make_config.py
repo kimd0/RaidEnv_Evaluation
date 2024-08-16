ab0cfde105f3e457b3790934ddf819ea919e3625
uwonsang <u.wonsang0514@gmail.com>
2024-08-16 15:17:30 +0900

    make config for gstar

--- gstar/make_config.py
+++ gstar/make_config.py
@@ -1,63 +1,80 @@
 import json
 import copy
+import argparse
+import itertools
+from tqdm import tqdm
 
 
-def read_json(file_name):
-    with open(file_name, 'r') as f:
-        data = json.load(f)
-    return data
+def parse_args():
+    parser = argparse.ArgumentParser('make gstar_config')
+    parser.add_argument('--json_path', type=str, default="./json/default_dealer_skill1.json")
+    parser.add_argument('--save_path', type=str, default="./config")
+    parser.add_argument('--agent_index', type=int, default=2)
+    parser.add_argument('--t_skill_num', type=int, default=0)
+
+    return parser.parse_args()
 
 
-def write_json(file_name, data):
-    with open(file_name, 'w') as f:
-        json.dump(data, f, indent=4)
+class ConfigManager:
+    def __init__(self, args):
+        self.args = args
+        self.source_file_name = args.json_path
+        self.target_directory = args.save_path
+        self.data = self.read_json()
 
+    def read_json(self):
+        with open(self.source_file_name, 'r') as f:
+            data = json.load(f)
+        return data
 
-def read_readme(file_name):
-    with open(file_name, 'r') as f:
-        data = f.read()
-    return data
+    def write_json(self, file_name, data):
+        with open(file_name, 'w') as f:
+            json.dump(data, f, indent=4)
 
+    def generate_configs(self, skillconfig):
+        origin_data = copy.deepcopy(self.data)
 
-def write_readme(file_name, data):
-    with open(file_name, 'a') as f:
-        f.write(data)
+        config_combi = list(itertools.product(
+            skillconfig["healthMax"],
+            skillconfig["armor"],
+            skillconfig["moveSpeed"],
+            skillconfig["cooltime"],
+            skillconfig["range"],
+            skillconfig["casttime"],
+            skillconfig["damage"]
+        ))
+
+        for i, (health_V, armor_V, move_V, cool_V, range_V, cast_V, damage_V) in tqdm(enumerate(config_combi),
+                                                                                      total=len(config_combi)):
+            new_data = copy.deepcopy(origin_data)
+
+            new_data['agentConfigs'][self.args.agent_index]['statusConfig']["healthMax"] = [health_V]
+            new_data['agentConfigs'][self.args.agent_index]['statusConfig']["armor"] = [armor_V]
+            new_data['agentConfigs'][self.args.agent_index]['statusConfig']["moveSpeed"] = [move_V]
+            new_data['agentConfigs'][self.args.agent_index]['skillConfigs'][self.args.t_skill_num]["cooltime"] = [cool_V]
+            new_data['agentConfigs'][self.args.agent_index]['skillConfigs'][self.args.t_skill_num]["range"] = [range_V]
+            new_data['agentConfigs'][self.args.agent_index]['skillConfigs'][self.args.t_skill_num]["casttime"] = [cast_V]
+            new_data['agentConfigs'][self.args.agent_index]['skillConfigs'][self.args.t_skill_num]["damage"] = [damage_V]
+
+            target_file_name = f'{self.target_directory}\\config_{i:05}.json'
+            self.write_json(target_file_name, new_data)
 
 
 if __name__ == "__main__":
-    # read json file
-    source_file_name = 'C:\\Users\\KKGB\\Desktop\\git_local_storage\\RaidEnv_PlayTesting\\MyTest\\config_generator\\default_config.json'
-    data = read_json(source_file_name)
+    args = parse_args()
 
-    playerConfig = {
-        0: ["Fireball", "Pyroblast"],
+    # Initialize ConfigManager
+    manager = ConfigManager(args)
+
+    skillConfig_list = {
+        "healthMax": [5000, 22000, 38000, 55000],
+        "armor": [18000, 31000, 44000, 58000],
+        "moveSpeed": [0.9, 1.1, 1.3, 1.5],
+        "cooltime": [0.1, 1.2, 2.4, 3.6],
+        "range": [3, 3.6, 4.3, 5],
+        "casttime": [0, 1.3, 2.6, 4],
+        "damage": [30000, 37000, 45000, 52000],
     }
 
-    skillConfig = {
-        "cooltime": [0.5, 6.5],
-        "range": [6, 13],
-        "casttime": [0, 3.5],
-        "damage": [9000, 16000],
-    }
-
-    i = 0
-    for coolvalue in skillConfig["cooltime"]:
-        for rangevalue in skillConfig["range"]:
-            for castvalue in skillConfig["casttime"]:
-                for damagevalue in skillConfig["damage"]:
-                    new_data = copy.deepcopy(data)
-                    agentConfigs = new_data["agentConfigs"]
-                    skillConfigs = agentConfigs[0]["skillConfigs"]
-
-                    skillConfigs[0]["cooltime"] = [coolvalue]
-                    skillConfigs[0]["range"] = [rangevalue]
-                    skillConfigs[0]["casttime"] = [castvalue]
-                    skillConfigs[0]["damage"] = [damagevalue]
-
-                    readme_data = f'{i:05} - range: {rangevalue}, cool: {coolvalue}, cast: {castvalue}, damage: {damagevalue} <br/> \n'
-
-                    # write json file
-                    target_file_name = f'C:\\Users\\KKGB\\Desktop\\git_local_storage\\RaidEnv_PlayTesting\\experiments\\exp_01\\test_configs\\config_{i:05}.json'
-                    write_json(target_file_name, new_data)
-                    write_readme('C:\\Users\\KKGB\\Desktop\\git_local_storage\\RaidEnv_PlayTesting\\experiments\\exp_01\\test_configs\\README.md', readme_data)
-                    i += 1
+    # Generate configurations
+    manager.generate_configs(skillConfig_list)
